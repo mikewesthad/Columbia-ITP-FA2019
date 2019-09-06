cb808ee0d038057751a51bb09bfb9223321904c2
Michael Hadley <michaelwesthadley@gmail.com>
2019-09-05 21:32:09 -0500

    Zip script that runs against gitignore

---
+++ zip-weekly-materials.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Files must be commited before running
+
+for dir in Weekly\ Materials/*/ ; do
+	folderpath=${dir%/} # Strip trailing slash
+  git archive --format=zip -o "$folderpath.zip" HEAD:"$dir"
+done
