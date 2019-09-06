6d2a70671fe4e4b7e6a7aa86f652627bcc3174ed
Michael Hadley <michaelwesthadley@gmail.com>
2019-09-06 08:39:41 -0500

    Add comment

--- zip-weekly-materials.sh
+++ zip-weekly-materials.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Files must be commited before running
+# Script to run zipping through git, effectively applying .gitignore to the zip. Files must be
+# commited before running.
 
 for dir in Weekly\ Materials/*/ ; do
 	folderpath=${dir%/} # Strip trailing slash
