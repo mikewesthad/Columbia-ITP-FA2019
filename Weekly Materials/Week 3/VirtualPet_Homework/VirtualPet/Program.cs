0c1bf45291f675bb7764849008da0dce3711c254
Michael Hadley <michaelwesthadley@gmail.com>
2019-09-19 16:53:17 -0500

    Week 3 code

---
+++ Weekly Materials/Week 3/VirtualPet_Homework/VirtualPet/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using static System.Console;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Title = "Virtual Pet Simulator - Mike Hadley";
+
+            ForegroundColor = ConsoleColor.DarkCyan;
+            BackgroundColor = ConsoleColor.White;
+            Clear();
+
+            string titleArt = @"
+ ,    /+.
+((___/ __>           \    /\
+/      }              )  ( ')
+\ .++.(    ___       (  /  )
+ \\   \\  /___\       \(__)+
+";
+            WriteLine(titleArt);
+            WriteLine("Welcome to the pet simulator. There are a few pets already here:");
+            
+            ForegroundColor = ConsoleColor.Black;
+            VirtualPet junior = new VirtualPet();
+            junior.Name = "Junior";
+            junior.AgeInDays = 7;
+            junior.Greet();
+
+            // Access a static method by using the class name, not an instance.
+            WriteLine();
+            VirtualPet.DisplayNumPetsCreated();
+            WriteLine();
+
+            VirtualPet ruffles = new VirtualPet();
+            ruffles.Name = "Ruffles";
+            ruffles.AgeInDays = 24;
+            ruffles.Mood = "Playful";
+            ruffles.Greet();
+
+            WriteLine();
+            VirtualPet.DisplayNumPetsCreated();
+            WriteLine();
+
+            ForegroundColor = ConsoleColor.DarkCyan;
+            Write("Now it's time for your own pet. Please name your pet: ");
+            string petName = ReadLine();
+            Write("What mood should your pet have: ");
+            string petMood = ReadLine();
+            Write("How hungry is your pet (0 - 100): ");
+            string petHungerResponse = ReadLine();
+
+            // Convert.ToInt32() is a static method that allows us to take a string and turn it 
+            // into an int. See: https://docs.microsoft.com/en-us/dotnet/api/system.convert.toint32
+            int petHunger = Convert.ToInt32(petHungerResponse);
+
+            VirtualPet playersPet = new VirtualPet(petName, petMood, 5, petHunger);
+            ForegroundColor = ConsoleColor.Black;
+            playersPet.Greet();
+
+            WriteLine();
+            VirtualPet.DisplayNumPetsCreated();
+            WriteLine();
+
+            // Art from https://www.asciiart.eu/animals
+            ForegroundColor = ConsoleColor.DarkCyan;
+            WriteLine("Art Credits:");
+            WriteLine("Dog with dog dish by Joan Stark");
+            WriteLine("Sitting cat by Joan Stark");
+
+            ReadKey();
+        }
+    }
+}
---
+++ Weekly Materials/Week 3/VirtualPet_Homework/VirtualPet/VirtualPet.cs
@@ -0,0 +1,53 @@
+﻿using static System.Console;
+
+namespace VirtualPet
+{
+    class VirtualPet
+    {
+        public string Name;
+        public string Mood;
+        public int AgeInDays;
+        public int HungerLevel;
+        public bool IsAwake = false;
+
+        // A static field lives on the class itself and NOT on the instances. Sometime you have 
+        // data about the class, but that wouldn't make sense to have on each instance, e.g. 
+        // tracking the total number of pets in our virtual game world.
+        static public int NumPetsCreated = 0;
+
+        // A static method lives on a class and NOT on the instances. Inside of a static method, 
+        // you can access static fields (e.g. NumPetsCreated) but not instance fields.
+        static public void DisplayNumPetsCreated()
+        {
+            WriteLine("There are currently " + NumPetsCreated + " pets alive in the world.");
+        }
+
+        // A constructor that takes no parameters and sets some of the fields to default values.
+        public VirtualPet()
+        {
+            Name = "Unknown";
+            Mood = "Unknown";
+            AgeInDays = 0;
+            HungerLevel = 0;
+            NumPetsCreated = NumPetsCreated + 1;
+        }
+
+        // A constructor that takes three parameters and uses them to set the fields.
+        public VirtualPet(string name, string mood, int ageInDays, int hungerLevel)
+        {
+            Name = name;
+            Mood = mood;
+            AgeInDays = ageInDays;
+            HungerLevel = hungerLevel;
+            NumPetsCreated = NumPetsCreated + 1;
+        }
+
+        public void Greet()
+        {
+            WriteLine("Why hello, my name is " + Name + ".");
+            WriteLine("I am " + AgeInDays + " days old.");
+            WriteLine("I am currently feeling: " + Mood + ".");
+            WriteLine("I am " + HungerLevel + "% hungry.");
+        }
+    }
+}
